1ec0ecd6db244f76e6405eeddefdefe72f92e4df
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-10-15 22:15:19 +0300

    Update #10.cpp

--- #10.cpp
+++ #10.cpp
@@ -38,33 +38,36 @@ bool isSpace(char c) {
 }
 
 bool checkFormula(const std::string& formula) {
-    std::stack<char> bracketsStack;
+    std::stack<char> bracketsStack; //создание стека для хранения открывающих скобок.
 
     for (size_t i = 0; i < formula.length(); i++) {
         char c = formula[i];
 
-        if (isOpeningBracket(c)) {
+        if (isOpeningBracket(c)) { //если текущий символ является открывающей скобкой, он помещается в стек `bracketsStack
             bracketsStack.push(c);
         }
-        else if (isClosingBracket(c)) {
+        else if (isClosingBracket(c)) { //Eсли текущий символ является закрывающей скобкой, выполняется следующая проверка:
+
+            
             if (bracketsStack.empty() || !isMatchingBracket(bracketsStack.top(), c)) {
-                return false;
-            }
-            bracketsStack.pop();
+                return false; // Если стек пустой или верхний элемент стека не соответствует текущей закрывающей скобке, возвращается `false`, так как это неправильная формула.
+            }                   //  - В противном случае, верхний элемент стека удаляется.
+            bracketsStack.pop(); 
         }
-        else if (isOperator(c)) {
+        else if (isOperator(c)) { // если текущий символ является оператором, выполняется следующая проверка:
             if (i == 0 || i == formula.length() - 1 || isOperator(formula[i - 1]) ||
                 isOperator(formula[i + 1]) || isSpace(formula[i - 1]) || isSpace(formula[i + 1])) {
-                return false;
+                return false; //- Если текущий оператор находится в начале или конце формулы, или перед ним или после него находится оператор, или перед ним или после него находится пробел, возвращается `false`, так как это неправильная формула.
             }
         }
-        else if (isOperand(c)) {
+        else if (isOperand(c)) { //если текущий символ является операндом, выполняется следующая проверка:
+
             if ((i > 0 && isOperand(formula[i - 1])) || (i < formula.length() - 1 && isOperand(formula[i + 1]))) {
-                return false;
+                return false; //-Если текущий операнд находится между двумя операндами, возвращается `false`, так как это неправильная формула.
             }
         }
-        else if (!isSpace(c)) {  // Проверка на неправильные символы
-            return false;
+        else if (!isSpace(c)) {  // если текущий символ не является пробелом и не попадает ни в одну из предыдущих категорий (открывающая/закрывающая скобка, оператор или операнд), возвращается `false`, так как это неправильная формула
+            return false; 
         }
     }
 
