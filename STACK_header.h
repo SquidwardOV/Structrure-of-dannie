3d24c611d11003233ae57ff6ea1c364f000c88b9
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-10-18 11:08:56 +0300

    Update STACK_header.h

--- STACK_header.h
+++ STACK_header.h
@@ -99,4 +99,25 @@ Stack::Stack(int* arr, int score) {
 	}
 }
 
+
 main.cpp
+
+	#include "Header.h"
+#include <iostream>
+
+int main() {
+	Stack stk;
+	stk.add(5);
+	stk.add(6);
+	stk.add(7);
+	stk.add(4);
+
+	std::cout << stk.show() << std::endl;
+	stk.remove();
+	std::cout << stk.show() << std::endl;
+	stk.remove();
+	std::cout << stk.show() << std::endl;
+	stk.remove();
+	std::cout << stk.show() << std::endl;
+	stk.remove();
+}
