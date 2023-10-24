be0d2ed90b8d5077e7c393ffbb962288cb8c163f
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-10-24 23:37:27 +0300

    Update templateStack.cpp

--- templateStack.cpp
+++ templateStack.cpp
@@ -1,140 +1,131 @@
-.h
-
-  #pragma once
-
 template <typename T>
-struct node
-{
-	node<T>* next;
-	int value;
-};
-
-template <typename T>
-struct Stack
-{
-	node<T>* head = new node;
+class Stack {
+private:
+	template <typename T>
+	class Node {
+	public:
+		T date;
+		Node* ptr;
+	};
 	int size = 0;
+	Node<T>* head = new Node<T>;
+public:
 
-	Stack(T* arr, T amount);
-
+	void add(T date);
+	T show();
+	void remove();
+	void clear();
+	Stack(T* arr, int score);
 	Stack();
-
 	~Stack();
-
-	void add_node(T value);
-
-	void delete_node();
-
-	T print_top();
-
-	void print_all();
 };
 
+template <typename T>
+Stack<T>::Stack(T* arr, int score) {
+	for (int i = 0; i < score; ++i) {
+		if (size == 0) {
+			head->date = arr[i];
+			size++;
+		}
+		else {
+			Node<T>* NewNode = new Node<T>;
+			NewNode->ptr = head->ptr;
+			NewNode->date = head->date;
+			head->ptr = NewNode;
+			head->date = arr[i];
+			size++;
+		}
+	}
+}
 
-.cpp
+template <typename T>
+Stack<T>::Stack() {
+
+}
+
+
+template <typename T>
+void Stack<T>::add(T date) {
+	if (head->ptr == nullptr && size == 0) {
+		head = new Node<T>;
+		head->date = date;
+		size++;
+	}
+	else {
+		Node<T>* NewNode = new Node<T>;
+		NewNode->ptr = head->ptr;
+		NewNode->date = head->date;
+		head->ptr = NewNode;
+		head->date = date;
+		size++;
+	}
+}
+
+template <typename T>
+void Stack<T>::remove() {
+	if (head->ptr == nullptr) {
+		size--;
+	}
+	else {
+		Node<T>* CurrentNode = new Node<T>;
+		CurrentNode = head;
+		head = head->ptr;
+		delete CurrentNode;
+		size--;
+	}
+}
+
+template <typename T>
+void Stack<T>::clear() {
+	while (size > 0) {
+		remove();
+	}
+}
+
+template <typename T>
+T Stack<T>::show() {
+	if (size == 0) {
+		throw;
+	}
+	else {
+		return head->date;
+	}
+
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+	clear();
+}
+
 
 #include "Stack.h"
 #include <iostream>
+#include <windows.h>
 
-template <typename T>
-Stack<T>::Stack(T* array, T amount)
-{
-	for (int i = 0; i < amount; i++)
-	{
-		if (size == 0)
-		{
-			head->next = new node;
-			(head->next)->value = array[i];
-			(head->next)->next = nullptr;
-			size++;
-		}
-		else
-		{
-			node* l = new node;
-			l->next = head->next;
-			l->value = head->value;
-			head->next = l;
-			head->value = array[i];
-			size++;
-		}
-	}
+struct Raccoon {
+    std::string name;
+    int age;
+    double weight;
+
+    Raccoon(std::string n, int a, double w) : name(n), age(a), weight(w) {}
+    Raccoon(){}
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    Stack<Raccoon> raccoon_stack;
+    raccoon_stack.add(Raccoon("Роки", 3, 4.5));
+    raccoon_stack.add(Raccoon("Глеб", 2, 3.8));
+    raccoon_stack.add(Raccoon("Бандит", 4, 5.2));
+
+    std::cout << "Енот в начале стека: " << raccoon_stack.show().name << std::endl;
+
+    raccoon_stack.remove();
+    std::cout << "Новый енот в начале: " << raccoon_stack.show().name << std::endl;
+
+    raccoon_stack.clear();
+    std::cout << "Стэк очищен" << std::endl;
+
+    return 0;
 }
-
-template <typename T>
-Stack<T>::Stack()
-{
-	head->next = nullptr;
-}
-
-template <typename T>
-Stack<T>::~Stack()
-{
-	while (size != 0)
-	{
-		delete_node();
-	}
-}
-
-template <typename T>
-void Stack<T>::add_node(int value)
-{
-	if (head->next == nullptr && size == 0) 
-	{
-		head = new node;
-		head->value = value;
-		size++;
-	}
-	else 
-	{
-		node* l = new node;
-		l->next = head->next;
-		l->value = head->value;
-		head->next = l;
-		head->value = value;
-		size++;
-	}
-}
-
-template <typename T>
-void Stack<T>::delete_node()
-{
-	if (head->next == nullptr) 
-	{
-		size--;
-	}
-	else 
-	{
-		node* l = new node;
-		l = head;
-		head = head->next;
-		delete l;
-		size--;
-	}
-}
-
-template <typename T>
-T Stack<T>::print_top()
-{
-	if (size == 0) 
-	{
-		return 0;
-	}
-	else 
-	{
-		return head->value;
-	}
-
-}
-
-template <typename T>
-void Stack<T>::print_all()
-{
-	node* l = head;
-	while ((l->next) != nullptr)
-	{
-		std::cout << l->value;
-		l = l->next;
-	}
-}
-
-
